bb8c3b7882ddfe28dbf9de220f436448c0362bf0
Timothy Ling <tim@homelab.cloud>
2021-08-26 16:27:06 -0600

    update generic zap class and zap utility command to align with existing maven code

--- src/ploigos_step_runner/step_implementers/zaproxy/zaproxy_generic.py
+++ src/ploigos_step_runner/step_implementers/zaproxy/zaproxy_generic.py
@@ -1,29 +1,55 @@
-from ploigos_step_runner import StepImplementer, StepResult, StepRunnerException
+"""Abstract parent class for StepImplementers that use ZAP.
+
+Step Configuration
+------------------
+Step configuration expected as input to this step.
+Could come from:
+* static configuration
+* runtime configuration
+* previous step results
+
+Configuration Key            | Required? | Default | Description
+-----------------------------|-----------|---------|-----------
+`tls-verify`                 | No        | `True`      | Disables TLS Verification if set to False
+`proxy-host`                 | No | `127.0.0.1` | Hostname/IP of the proxy
+`proxy-port`                 | No | `8080`    | Port of the proxy
+`zaproxy-api-key` | No        | `00000000-0000-0000-0000-000000000000`  | api key for zap
+"""
+
+
+from ploigos_step_runner import StepResult, StepRunnerException
+from ploigos_step_runner.config.config_value import ConfigValue
+from ploigos_step_runner.step_implementer import StepImplementer
+from ploigos_step_runner.utils.zap import run_zap
 
 DEFAULT_CONFIG = {
+    'tls-verify': True,
     'proxy-host': '127.0.0.1',
     'proxy-port': 8080,
     'zaproxy-api-key': '95993b18-0698-11ec-82a6-5f29471b38af'
 }
 
+REQUIRED_CONFIG_OR_PREVIOUS_STEP_RESULT_ARTIFACT_KEYS = [ 
+]
 
 class ZAProxyGeneric(StepImplementer):
+    """Abstract parent class for StepImplementers that use Maven.
     """
 
-    """
-
-    def __init__(
-            self,
-            workflow_result,
-            parent_work_dir_path,
-            config,
-            environment=None
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        workflow_result,
+        parent_work_dir_path,
+        config,
+        environment=None
     ):
-        super().__init__(workflow_result, parent_work_dir_path, config, environment)
+        super().__init__(
+            workflow_result=workflow_result,
+            parent_work_dir_path=parent_work_dir_path,
+            config=config,
+            environment=environment
+        )
 
-    @staticmethod
-    def _required_config_or_result_keys():
-        pass
 
     @staticmethod
     def step_implementer_config_defaults():
@@ -38,20 +64,108 @@ class ZAProxyGeneric(StepImplementer):
         -----
         These are the lowest precedence configuration values.
         """
-        return {**ZAProxyGeneric.step_implementer_config_defaults(), **DEFAULT_CONFIG}
+        return DEFAULT_CONFIG
 
-    def _run_zap_command(self, args):
-        return
+    @staticmethod
+    def _required_config_or_result_keys():
+        """Getter for step configuration or previous step result artifacts that are required before
+        running this step.
 
-    def _run_step(self):
+        See Also
+        --------
+        _validate_required_config_or_previous_step_result_artifact_keys
+
+        Returns
+        -------
+        array_list
+            Array of configuration keys or previous step result artifacts
+            that are required before running the step.
+        """
+        return REQUIRED_CONFIG_OR_PREVIOUS_STEP_RESULT_ARTIFACT_KEYS
+
+
+    def _validate_required_config_or_previous_step_result_artifact_keys(self):
+        """Validates that the required configuration keys or previous step result artifacts
+        are set and have valid values.
+
+        Validates that:
+        * required configuration is given
+        * given 'pom-file' exists
+
+        Raises
+        ------
+        AssertionError
+            If step configuration or previous step result artifacts have invalid required values
+        """
+        super()._validate_required_config_or_previous_step_result_artifact_keys()
+
+
+    def _run_zap_command(
+        self,
+        zap_output_file_path,
+        step_implementer_additional_arguments=None
+    ):
+      """Runs zap using the configuration given to this step runner.
+
+        Parameters
+        ----------
+        zap_output_file_path : str
+            Path to file containing the maven stdout and stderr output.
+        step_implementer_additional_arguments : []
+            Additional arguments hard coded by the step implementer.
+
+        Raises
+        ------
+        StepRunnerException
+            If zap returns a none 0 exit code.
+        """
+
+        zap_host = self.get_value('proxy-host')
+        zap_port = self.get_value('proxy-port')
+        zap_apikey = self.get_value('zaproxy-api-key')
+
+        additional_arguments = []
+        if step_implementer_additional_arguments:
+            additional_arguments = \ 
+                step_implementer_additional_arguments + self.get_value('zap-additional-arguments')
+        else:
+            additional_arguments = self.get_value('zap-additional-arguments')
+
+
+        run_zap(
+            zap_host=zap_host,
+            zap_port=zap_port,
+            additional_arguments=additional_arguments,
+            zap_output_file_path=zap_output_file_path
+        )
+        
+    def _run_step(self): # pylint: disable=too-many-locals
+        """Runs the step implemented by this StepImplementer.
+
+        Returns
+        -------
+        StepResult
+            Object containing the dictionary results of this step.
+        """
         step_result = StepResult.from_step_implementer(self)
 
+        #package the artifacts
+        zap_output_file_path = self.write_working_file('zap_output.txt')
         try:
             self._run_zap_command(
-                args=["-daemon"]
+                zap_host=zap_host,
+                zap_port=zap_port,
+                zap_output_file_path=zap_output_file_path
             )
         except StepRunnerException as error:
             step_result.success = False
             step_result.message = "Error executing ZAProxy command" \
                 f"More details may be found in 'zap output' report artifact: {error}"
-            return
+        finally:
+            step_result.add_artifact(
+                description="Standard out from ZAP",
+                name='zap-output',
+                value=zap_output_file_path
+            )
+
+         return step_result
---
+++ src/ploigos_step_runner/utils/zap.py
@@ -0,0 +1,60 @@
+"""Shared utils for zap operations.
+"""
+import os
+import sys
+
+import sh
+from ploigos_step_runner.exceptions import StepRunnerException
+from ploigos_step_runner.utils.io import \
+    create_sh_redirect_to_multiple_streams_fn_callback
+
+
+def run_zap( #pylint: disable=too-many-arguments
+    zap_host,
+    zap_proxy,
+    zap_output_file_path,
+    additional_arguments=None
+):
+    """Runs zap using the given configuration.
+
+    Parameters
+    ----------
+    zap_output_file_path : str
+        Path to file containing the maven stdout and stderr output.
+    additional_arguments : [str]
+        List of additional arguments to use.
+
+    Raises
+    ------
+    StepRunnerException
+        If zap returns a none 0 exit code.
+    """
+
+    if not additional_arguments:
+        additional_arguments = []
+
+    # run zap 
+    try:
+        with open(zap_output_file_path, 'w') as zap_output_file:
+            out_callback = create_sh_redirect_to_multiple_streams_fn_callback([
+                sys.stdout,
+                zap_output_file
+            ])
+            err_callback = create_sh_redirect_to_multiple_streams_fn_callback([
+                sys.stderr,
+                zap_output_file
+            ])
+
+            zapcmd = sh.Command("/zap/zap.sh")
+            zapcmd( # pylint: disable=no-member
+                '-daemon',
+                '-host', zap_host,
+                '-port', zap_proxy,
+                *additional_arguments,
+                _out=out_callback,
+                _err=err_callback
+            )
+    except sh.ErrorReturnCode as error:
+        raise StepRunnerException(
+            f"Error running zap. {error}"
+        ) from error
