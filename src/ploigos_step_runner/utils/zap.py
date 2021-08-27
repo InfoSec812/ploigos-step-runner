218d801a71243933e15b913df2f9cefc6a126b53
Deven Phillips <deven.phillips@redhat.com>
2021-08-27 13:00:17 -0400

    Fixed unit test

--- src/ploigos_step_runner/utils/zap.py
+++ src/ploigos_step_runner/utils/zap.py
@@ -36,7 +36,6 @@ def run_zap( #pylint: disable=too-many-arguments
     # run zap
     try:
         with open(zap_output_file_path, 'w') as zap_output_file:
-            print(zap_output_file)
             out_callback = create_sh_redirect_to_multiple_streams_fn_callback([
                 sys.stdout,
                 zap_output_file
--- tests/utils/test_zap.py
+++ tests/utils/test_zap.py
@@ -16,18 +16,18 @@ from tests.helpers.test_utils import Any
 
 
 class TestZapUtils_run_zap(BaseTestCase):
-    @patch('sh.__init__')
+    @patch('sh.Command')
     @patch('ploigos_step_runner.utils.zap.create_sh_redirect_to_multiple_streams_fn_callback')
     @patch("builtins.open", new_callable=mock_open)
-    def test_success_defaults(self, mocksh, mock_open, redirect_mock):
-        call_mock = mock.Mock()
-        mocksh.Command = call_mock
+    def test_success_defaults(self, mock_open, redirect_mock, mocksh):
+
+        file_descriptor = mock.Mock()
+        mock_open().return_value = file_descriptor
 
         with TempDirectory() as temp_dir:
             zap_output_file_path = os.path.join(temp_dir.path, 'zap_output.txt')
             zap_host = "127.0.0.1"
             zap_port = "8080"
-             
 
             run_zap(
                 zap_host=zap_host,
@@ -35,30 +35,27 @@ class TestZapUtils_run_zap(BaseTestCase):
                 zap_output_file_path=zap_output_file_path,
             )
 
-            file_descriptor = mock.Mock()
-            mock_open.side_effect = file_descriptor
-
-            # mock_open.assert_called_with(zap_output_file_path, 'w')
-            # redirect_mock.assert_has_calls([
-            #     call([
-            #         sys.stdout,
-            #         file_descriptor
-            #     ]),
-            #     call([
-            #         sys.stderr,
-            #         file_descriptor
-            #     ])
-            # ])
+            mock_open.assert_called_with(zap_output_file_path, 'w')
+            redirect_mock.assert_has_calls([
+                call([
+                    sys.stdout,
+                    mock.ANY
+                ]),
+                call([
+                    sys.stderr,
+                    mock.ANY
+                ])
+            ])
 
             mocksh.assert_called_once_with(
                 '/zap/zap.sh'
             )
 
-            call_mock.assert_called_once_with(
+            mocksh().assert_called_once_with(
                 '-daemon',
                 '-dir', '/home/.ZAP',
-                '-host', '/fake/pom.xml',
-                '-port', '/fake/settings.xml',
+                '-host', zap_host,
+                '-port', zap_port,
                 _out=Any(StringIO),
                 _err=Any(StringIO)
             )
